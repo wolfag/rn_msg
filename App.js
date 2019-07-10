d6110a7ed65373ef33981c50c762f47ddf2090d1
TaiNguyen <tainguyen@meditekcorp.com.vn>
2019-07-10 18:36:53 +0800

    init project

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+class App extends React.Component {
+  renderMessageList() {
+    return <View style={styles.content} />;
+  }
+
+  renderInputMethodEditor() {
+    return <View style={styles.inputMethodEditor} />;
+  }
+
+  renderToolbar() {
+    return <View style={styles.toolbar} />;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.renderMessageList()}
+        {this.renderToolbar()}
+        {this.renderInputMethodEditor()}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: "rgba(0,0,0,0.4)",
+    backgroundColor: "white"
+  }
+});
+
+export default App;
